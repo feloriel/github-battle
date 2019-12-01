9d3488073c5a7e13131e17897af19693a98d07ad
Irina Lepekhina <irina.lepekhina95@gmail.com>
2019-12-02 00:16:44 +0200

    created component, set up webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const webpack = require('webpack')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './app/index.js',
-  module: {
-    rules: [
-      { test: /\.svg$/, use: 'svg-inline-loader' },
-      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
-      { test: /\.(js)$/, use: 'babel-loader' }
-    ]
-  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index_bundle.js'
   },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] }
+    ]
+  },
+  mode: 'development',
   plugins: [
-    new HtmlWebpackPlugin()
-  ],
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
+    new HtmlWebpackPlugin({
+      template: 'app/index.html'
+    })
+  ]
 }
