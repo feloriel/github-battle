02a94149388e85e99f781bf25f3d63c9f62ccba5
Irina Lepekhina <irina.lepekhina95@gmail.com>
2019-12-18 15:09:17 +0200

    added reset players button

--- app/components/Results.js
+++ app/components/Results.js
@@ -82,26 +82,39 @@ export default class Results extends React.Component {
     }
 
     return (
-      <div className='grid space-around container-sm'>
-        <Card 
-          header={winner.score === loser.score ? 'Tie' : 'Winner'}
-          subheader={`Score: ${winner.score.toLocaleString()}`}
-          avatar={winner.profile.avatar_url}
-          name={winner.profile.login}
-          href={winner.profile.html_url}
-        >
-          <ProfileList profile={winner.profile} />
-        </Card>
-        <Card
-          header={winner.score === loser.score ? 'Tie' : 'Loser'}
-          subheader={`Score: ${loser.score.toLocaleString()}`}
-          avatar={loser.profile.avatar_url}
-          name={loser.profile.login}
-          href={loser.profile.html_url}
-        >
-          <ProfileList profile={loser.profile} />
-        </Card>
-      </div>
+      <React.Fragment>
+        <div className='grid space-around container-sm'>
+          <Card 
+            header={winner.score === loser.score ? 'Tie' : 'Winner'}
+            subheader={`Score: ${winner.score.toLocaleString()}`}
+            avatar={winner.profile.avatar_url}
+            name={winner.profile.login}
+            href={winner.profile.html_url}
+          >
+            <ProfileList profile={winner.profile} />
+          </Card>
+          <Card
+            header={winner.score === loser.score ? 'Tie' : 'Loser'}
+            subheader={`Score: ${loser.score.toLocaleString()}`}
+            avatar={loser.profile.avatar_url}
+            name={loser.profile.login}
+            href={loser.profile.html_url}
+          >
+            <ProfileList profile={loser.profile} />
+          </Card>
+        </div>
+        <button
+          onClick={this.props.onReset}
+          className='btn dark-btn btn-space'>
+            Reset
+        </button>
+      </React.Fragment>
     )
   }
+}
+
+Results.propTypes = {
+  PlayerOne: PropTypes.string.isRequired,
+  PlayerTwo: PropTypes.string.isRequired,
+  onReset: PropTypes.func.isRequired
 }
